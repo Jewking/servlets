ad490050c02927a60e09b7ce7702b261c1f9caae
Jewking <csl1139753@gmail.com>
2021-03-28 13:14:29 +0300

    Changes

--- src/main/java/com/anderson/servlets/FrontControllerServlet.java
+++ src/main/java/com/anderson/servlets/FrontControllerServlet.java
@@ -1,5 +1,7 @@
 package com.anderson.servlets;
 
+import java.io.PrintWriter;
+
 import com.anderson.servlets.commands.FrontCommand;
 import com.anderson.servlets.commands.UnknownCommand;
 
@@ -12,9 +14,18 @@ import java.io.IOException;
 public class FrontControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        FrontCommand command = getCommand(request);
-        command.init(getServletContext(), request, response);
-        command.execute();
+        PrintWriter writer = response.getWriter();
+
+        String cmd = request.getParameter("command");
+
+        try {
+            writer.println("<h2>COMMAND: " + cmd + "</h2><br><p>response: " + response + "  </p>");
+        } finally {
+            writer.close();
+        }
+//        FrontCommand command = getCommand(request);
+//        command.init(getServletContext(), request, response);
+//        command.execute();
     }
 
 //    @Override
