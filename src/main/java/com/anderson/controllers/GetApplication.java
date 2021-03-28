ffcbfddef9b8fc395dbe0d29f537afad30a61bd1
Jewking <csl1139753@gmail.com>
2021-03-28 20:36:52 +0300

    Changes

---
+++ src/main/java/com/anderson/controllers/GetApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetApplication {
+
+    public static void main(String[] args) {
+        WebConfiguration webConfiguration = new WebConfiguration();
+        SpringApplication.run(GetApplication.class, args);
+    }
+}
